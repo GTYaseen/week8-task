7068e0522855d6d0a736f645ef6e7d223b370ed5
GTYaseen <Xxyaseent11xX14@gmail.com>
2023-12-21 13:06:03 +0300

    done

--- models/dashboard/products-A.js
+++ models/dashboard/products-A.js
@@ -1,8 +1,26 @@
 const client = require("../../db");
 
 async function ViewProducts(req, res) {
-  const result = await client.query(`SELECT * FROM products`);
-  res.send(result.rows);
+  try {
+    let { search, limit } = req.query;
+    let query = "SELECT * FROM products";
+
+    if (search) {
+      query += " WHERE name ILIKE $1";
+      search = `%${search}%`;
+    }
+
+    if (limit) {
+      query += " LIMIT $2";
+    }
+
+    const result = await client.query(query, limit ? [search, limit] : [search]);
+
+    res.send(result.rows);
+  } catch (error) {
+    console.error("Error retrieving products:", error);
+    res.status(500).send("Internal Server Error");
+  }
 }
 async function AddProduct(req, res) {
   const { name, price, discount, image, active } = req.body;
